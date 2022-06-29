6720640ce3e0c4a6a081bb95c01247940e898dd2
Dmitriy Petrov <nevermind1979@mail.ru>
2022-06-29 19:32:53 +0300

    add project

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,10 @@
  * DATE: 29.06.2022
  */
 public class Main {
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener,errorListener);
+        worker.start();
+    }
 }
